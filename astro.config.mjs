9f8337ab913fc95449d60f1f10cebec374ac3d9b
Satwik <satwikmidya@gmail.com>
2025-12-24 20:57:26 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://github.com/SatwikMidya/.github.io",
+    site: "https://github.com/SatwikMidya/portfolio.github.io",
     base: "/portfolio/",
     output: "static",
 });
